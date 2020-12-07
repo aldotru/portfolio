0e2120cb24e436ce14801eea07dcc764e19bd1ae
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-12-07 13:59:53 -0600

    Slick carousel opts for projects and testimonials sliders

---
+++ js/slick.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  
+  // Slick carousel options
+
+  // Projects carousel
+  $('.projects__carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 7500,
+    arrows: true,
+    centerMode: true,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 300,
+    variableWidth: true,
+
+    // Moving next/previous buttons outside the slick wrapper
+    // @type {object} DOM node
+    nextArrow: $('.projects__carousel-arrow--right'),
+    prevArrow: $('.projects__carousel-arrow--left')
+  });
+
+  // Testimonials carousel
+  $('.testimonials__carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 7500,
+    arrows: true,
+    centerMode: true,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+
+    // Moving next/previous buttons outside the slick wrapper
+    // @type {object} DOM node
+    nextArrow: $('.testimonials__arrow--right'),
+    prevArrow: $('.testimonials__arrow--left')
+  });
+
+});
