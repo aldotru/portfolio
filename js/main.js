35a8d6174a006d79e049c4263a1957f5ff092be0
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-12-09 16:06:07 -0600

    Uncommented projects slider

--- js/main.js
+++ js/main.js
@@ -7,22 +7,22 @@ Marquee3k.init();
 
 // Slick carousel options
 // Projects carousel
-// $(".projects__carousel").slick({
-//   autoplay: true,
-//   autoplaySpeed: 7500,
-//   arrows: true,
-//   centerMode: true,
-//   draggable: true,
-//   infinite: true,
-//   slidesToShow: 2,
-//   slidesToScroll: 1,
-//   speed: 300,
-//   variableWidth: true,
-//   // Moving next/previous buttons outside the slick wrapper
-//   // @type {object} DOM node
-//   nextArrow: $('.projects__carousel-arrow--right'),
-//   prevArrow: $('.projects__carousel-arrow--left')
-// });
+$(".projects__carousel").slick({
+  autoplay: true,
+  autoplaySpeed: 7500,
+  arrows: true,
+  centerMode: true,
+  draggable: true,
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  speed: 300,
+  variableWidth: true,
+  // Moving next/previous buttons outside the slick wrapper
+  // @type {object} DOM node
+  nextArrow: $('.projects__carousel-arrow--right'),
+  prevArrow: $('.projects__carousel-arrow--left')
+});
 
 // Testimonials carousel
 $(".testimonials__carousel").slick({
