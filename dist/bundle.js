fc52d725811b27ee5b3f07de923f48a2276be3c7
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-12-09 15:43:25 -0600

    Bundled js

--- dist/bundle.js
+++ dist/bundle.js
@@ -8,39 +8,39 @@ Marquee3k.init();
 
 // Slick carousel options
 // Projects carousel
-$(".projects__carousel").slick({
-  autoplay: true,
-  autoplaySpeed: 7500,
-  arrows: true,
-  centerMode: true,
-  draggable: true,
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  speed: 300,
-  variableWidth: true,
-  // Moving next/previous buttons outside the slick wrapper
-  // @type {object} DOM node
-  nextArrow: $('.projects__carousel-arrow--right'),
-  prevArrow: $('.projects__carousel-arrow--left')
-});
-
-// Testimonials carousel
-// $(".testimonials__carousel").slick({
+// $(".projects__carousel").slick({
 //   autoplay: true,
 //   autoplaySpeed: 7500,
 //   arrows: true,
 //   centerMode: true,
 //   draggable: true,
 //   infinite: true,
-//   slidesToShow: 1,
+//   slidesToShow: 2,
 //   slidesToScroll: 1,
 //   speed: 300,
+//   variableWidth: true,
 //   // Moving next/previous buttons outside the slick wrapper
 //   // @type {object} DOM node
-//   nextArrow: $('.testimonials__arrow--right'),
-//   prevArrow: $('.testimonials__arrow--left')
+//   nextArrow: $('.projects__carousel-arrow--right'),
+//   prevArrow: $('.projects__carousel-arrow--left')
 // });
+
+// Testimonials carousel
+$(".testimonials__carousel").slick({
+  autoplay: true,
+  autoplaySpeed: 7500,
+  arrows: true,
+  centerMode: true,
+  draggable: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 300,
+  // Moving next/previous buttons outside the slick wrapper
+  // @type {object} DOM node
+  nextArrow: $('.testimonials__arrow--right'),
+  prevArrow: $('.testimonials__arrow--left')
+});
 },{"jquery":2,"marquee3000":3,"slick-carousel":4}],2:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v3.5.1
