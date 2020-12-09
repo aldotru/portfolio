d8fb750695cbf70fbd4818a9d90749bced854355
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-12-09 15:43:09 -0600

    Commented projects slider for testing

--- js/main.js
+++ js/main.js
@@ -7,36 +7,36 @@ Marquee3k.init();
 
 // Slick carousel options
 // Projects carousel
-$(".projects__carousel").slick({
-  autoplay: true,
-  autoplaySpeed: 7500,
-  arrows: true,
-  centerMode: true,
-  draggable: true,
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  speed: 300,
-  variableWidth: true,
-  // Moving next/previous buttons outside the slick wrapper
-  // @type {object} DOM node
-  nextArrow: $('.projects__carousel-arrow--right'),
-  prevArrow: $('.projects__carousel-arrow--left')
-});
-
-// Testimonials carousel
-// $(".testimonials__carousel").slick({
+// $(".projects__carousel").slick({
 //   autoplay: true,
 //   autoplaySpeed: 7500,
 //   arrows: true,
 //   centerMode: true,
 //   draggable: true,
 //   infinite: true,
-//   slidesToShow: 1,
+//   slidesToShow: 2,
 //   slidesToScroll: 1,
 //   speed: 300,
+//   variableWidth: true,
 //   // Moving next/previous buttons outside the slick wrapper
 //   // @type {object} DOM node
-//   nextArrow: $('.testimonials__arrow--right'),
-//   prevArrow: $('.testimonials__arrow--left')
+//   nextArrow: $('.projects__carousel-arrow--right'),
+//   prevArrow: $('.projects__carousel-arrow--left')
 // });
+
+// Testimonials carousel
+$(".testimonials__carousel").slick({
+  autoplay: true,
+  autoplaySpeed: 7500,
+  arrows: true,
+  centerMode: true,
+  draggable: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 300,
+  // Moving next/previous buttons outside the slick wrapper
+  // @type {object} DOM node
+  nextArrow: $('.testimonials__arrow--right'),
+  prevArrow: $('.testimonials__arrow--left')
+});
