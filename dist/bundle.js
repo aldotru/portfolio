60b9d7545b28edc988fb4e93959a56c87a9af2f6
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-12-12 09:18:30 -0600

    Bundled js

--- dist/bundle.js
+++ dist/bundle.js
@@ -10,7 +10,7 @@ Marquee3k.init();
 // Projects carousel
 $(".projects__carousel").slick({
   autoplay: true,
-  autoplaySpeed: 7500,
+  autoplaySpeed: 5000,
   arrows: true,
   centerMode: true,
   draggable: true,
@@ -28,7 +28,7 @@ $(".projects__carousel").slick({
 // Testimonials carousel
 $(".testimonials__carousel").slick({
   autoplay: true,
-  autoplaySpeed: 7500,
+  autoplaySpeed: 5000,
   arrows: true,
   centerMode: true,
   draggable: true,
